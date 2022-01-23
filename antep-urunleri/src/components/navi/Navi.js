d7a43cfbf2c12f7a15da3a3e862bb867213ea582
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-01-23 10:02:26 +0300

    ürün ekleme sayfası eklendi

--- antep-urunleri/src/components/navi/Navi.js
+++ antep-urunleri/src/components/navi/Navi.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import {
   Collapse,
   Navbar,
@@ -28,42 +29,40 @@ export default class Navi extends React.Component {
 
   render() {
     return (
-      <div >
-        <Navbar  light expand="md">
-          <NavbarBrand href="/">
-            {" "}
-             <img
-              src="https://imgyukle.com/f/2022/01/10/oNjPky.png"
-              width={100}
-              height={100}
-            />{" "} 
+      <div>
+        <Navbar light expand="md">
+          <NavbarBrand>
+            <Link to="/">
+              <img
+                src="https://imgyukle.com/f/2022/01/10/oNjPky.png"
+                width={100}
+                height={100}
+              />
+            </Link>{" "}
           </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="https://www.linkedin.com/in/busrayalinkilic/">
-                  LinkedIn
+                <NavLink href="http://localhost:3001/saveproduct">
+                  Ürün Kayıt
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="https://github.com/busrayalinkilic">
                   GitHub
                 </NavLink>
-              
               </NavItem>
 
-              <CartSummary></CartSummary>
               <NavItem>
-                <NavLink href="https://imgyukle.com/f/2022/01/21/omsDxI.png">İletişim Bilgileri</NavLink>
+                <NavLink href="https://imgyukle.com/f/2022/01/21/omsDxI.png">
+                  İletişim Bilgileri
+                </NavLink>
               </NavItem>
-              
+              <CartSummary></CartSummary>
             </Nav>
           </Collapse>
         </Navbar>
-
-            
-        
 
         <br></br>
         <br></br>
